2619788bbd17631aa7a8b415e40f0c3117a297b5
Martín Mallea <martin.mallea@wolox.com.ar>
2019-05-09 16:43:31 -0300

    Deleting scss lint

--- script/pre-push.sh
+++ script/pre-push.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# echo "\033[34mRunning Rspec tests\033[0m"
-# RUN_CHECK_CMD='bundle exec rspec spec -fd'
-# RUN_TESTS_OUTPUT=`${RUN_CHECK_CMD}`
+echo "\033[34mRunning Rspec tests\033[0m"
+RUN_CHECK_CMD='bundle exec rspec spec -fd'
+RUN_TESTS_OUTPUT=`${RUN_CHECK_CMD}`
 
 if [ $? -eq 1 ]
 then
