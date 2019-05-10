285ea9d057bb466fbbac45cb55bc25f1c819c053
Martín Mallea <martin.mallea@wolox.com.ar>
2019-05-10 18:29:37 -0300

    Routes.rb resources grouping

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'sessions'
-  get '/users', to: 'users#index'
-  get '/users/me', to:'users#me'
-  get '/users/:id', to: 'users#show'
-  patch '/users/:id', to: 'users#update'
-  post '/users', to: 'users#create'
+  resource :users, only: [:index, :show, :create, :update]
+  get '/users/me', to: 'users#me'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
