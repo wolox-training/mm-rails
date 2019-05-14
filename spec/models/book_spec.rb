ab16836a136fe1a8e1bf4761248a2f04840ada36
Martín Mallea <martin.mallea@wolox.com.ar>
2019-05-14 16:10:03 -0300

    Adding books tests

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -2,12 +2,33 @@ require 'rails_helper'
 
 describe Book do
   subject(:book) { build(:book) }
+  subject(:invalid_year_book) { build(:book, year: '125.4') }
+  subject(:missing_author_book) { build(:book, author: nil) }
+  subject(:empty_title_book) { build(:book, title: '') }
 
-  describe '#id' do
+  describe '#save' do
     context 'When the book is created' do
       it 'generates an id' do
         book.save!
         expect(book.id).to be_present
+      end
+    end
+
+    context 'when year is invalid' do
+      it 'fails at save' do
+        expect { invalid_year_book.save! }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+
+    context 'when author is missing' do
+      it 'fails at save' do
+        expect { missing_author_book.save! }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+
+    context 'when title is empty' do
+      it 'fails at save' do
+        expect { missing_author_book.save! }.to raise_error(ActiveRecord::RecordInvalid)
       end
     end
   end
