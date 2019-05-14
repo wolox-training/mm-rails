a11c633d925b47153ee9f7da98777c9664878329
Martín Mallea <martin.mallea@wolox.com.ar>
2019-05-14 14:43:24 -0300

    Book factory

---
+++ spec/factories/book_factory.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :book do
+    author { 'Emmie Thiel' }
+    title { 'Ring of Bright Water' }
+    publisher { 'Butterworth-Heinemann' }
+    year { '1968' }
+    genre { 'Short story' }
+    image { 'fjfwieo' }
+  end
+end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,11 +3,7 @@ require 'rails_helper'
 describe Book do
   subject(:book) { build(:book) }
 
-  # it { is_expected.to be_valid }
-  # it { is_expected.to validate_presence_of(:first_name) }
-  # it { is_expected.to validate_presence_of(:last_name) }
-
-  describe '#generate_verification_code' do
+  describe '#id' do
     context 'When the book is created' do
       it 'generates an id' do
         book.save!
