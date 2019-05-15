656eab622958b6f9f656ef28d1541552fa7ed00c
Martín Mallea <martin.mallea@wolox.com.ar>
2019-05-15 13:24:23 -0300

    Adding shoulda validations on Book model test

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 describe Book do
   subject(:book) { build(:book) }
+  it { is_expected.to be_valid }
+  it { should validate_presence_of :genre }
+  it { should validate_presence_of :author }
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :publisher }
+  it { should validate_presence_of :image }
+  it { should validate_numericality_of(:year).only_integer }
 
   describe '#save' do
     subject(:save) { book.save }
