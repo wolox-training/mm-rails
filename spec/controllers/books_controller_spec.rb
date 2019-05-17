c3fd43dc7a53b09fa2b003e958463f9148fee651
Martín Mallea <martin.mallea@wolox.com.ar>
2019-05-17 15:23:08 -0300

    Adding filtering tests for /books endpoint

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -2,10 +2,6 @@ require 'rails_helper'
 require_relative 'shared_context'
 
 describe BooksController do
-  def response_body
-    JSON.parse(response.body)
-  end
-
   it { is_expected.to route(:get, '/books').to(action: :index) }
   it { is_expected.to route(:get, '/books/1').to(action: :show, id: 1) }
 
@@ -28,13 +24,13 @@ describe BooksController do
 
         it 'responses with the books json' do
           expected = ActiveModel::Serializer::CollectionSerializer.new(
-            books, each_serializer: BookPreviewSerializer
+            books, serializer: BookPreviewSerializer
           ).to_json
-          expect(response_body['page']) =~ JSON.parse(expected)
+          expect(response_body['page']).to eq JSON.parse(expected)
         end
       end
 
-      context 'When fetching with a limit=2 parameter' do
+      context 'When params[:limit] == 2' do
         before { get :index, params: { limit: 2 } }
 
         it { is_expected.to respond_with :ok }
@@ -49,6 +45,81 @@ describe BooksController do
 
         it 'responses with a 2 books json' do
           expect(response_body['page'].length).to eq 2
+        end
+      end
+
+      context 'When params[:genre] == Action' do
+        before do
+          create_list(:book, 5, genre: 'Action')
+          create_list(:book, 5, genre: 'Fantasy')
+          get :index, params: { genre: 'Action' }
+        end
+        subject { response_body }
+
+        it { is_expected.to be_paginated }
+
+        it 'responses with a json with 5 books' do
+          expect(response_body['page'].length).to eq 5
+        end
+        it 'returns only books with genre == Action' do
+          expect(response_body['page'].map { |book| book['genre'] }).to satisfy do |genres|
+            genres.all? { |genre| genre == 'Action' }
+          end
+        end
+      end
+
+      context 'When params[:author] == Bradford Nolan DDS' do
+        before do
+          create_list(:book, 2, author: 'Bradford Nolan DDS')
+          create_list(:book, 1, author: 'Luis Miguel')
+          get :index, params: { author: 'Bradford Nolan DDS' }
+        end
+        subject { response_body }
+
+        it { is_expected.to be_paginated }
+        it 'responses with a json with 2 books' do
+          expect(response_body['page'].length).to eq 2
+        end
+        it 'returns only books with author == Bradford Nolan DDS' do
+          expect(response_body['page'].map { |book| book['author'] }).to satisfy do |authors|
+            authors.all? { |author| author == 'Bradford Nolan DDS' }
+          end
+        end
+      end
+
+      context 'When params[:author] == Bradford Nolan DDS' do
+        before do
+          create_list(:book, 2, author: 'Bradford Nolan DDS')
+          create_list(:book, 1, author: 'Luis Miguel')
+          get :index, params: { author: 'Bradford Nolan DDS' }
+        end
+        subject { response_body }
+
+        it { is_expected.to be_paginated }
+        it 'responses with a json with 2 books' do
+          expect(response_body['page'].length).to eq 2
+        end
+        it 'returns only books with author == Bradford Nolan DDS' do
+          expect(response_body['page'].map { |book| book['author'] }).to satisfy do |authors|
+            authors.all? { |author| author == 'Bradford Nolan DDS' }
+          end
+        end
+      end
+
+      context 'When params[:title] == La biblia!' do
+        before do
+          create(:book, title: 'La biblia!')
+          create(:book, title: 'Harry Potter')
+          get :index, params: { title: 'La biblia!' }
+        end
+        subject { response_body }
+
+        it { is_expected.to be_paginated }
+        it 'responses with a json with 1 book' do
+          expect(response_body['page'].length).to eq 1
+        end
+        it 'returns only a book with title == La biblia!' do
+          expect(response_body['page'][0]['title']).to eq 'La biblia!'
         end
       end
     end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require_relative 'support/factory_bot'
+require_relative 'support/parsed_response_helper'
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
@@ -62,6 +63,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Response::JSONParser, type: :controller
 end
 
 Shoulda::Matchers.configure do |config|
