4e1fad09885ec3abc692365ff8b1dbb1490a07fc
Martín Mallea <martin.mallea@wolox.com.ar>
2019-05-20 16:08:01 -0300

    Adding rents controller and routes

---
+++ app/controllers/rents_controller.rb
@@ -0,0 +1,19 @@
+class RentsController < ApplicationController
+  def index
+    render_paginated json: current_user.rents
+  end
+
+  def create
+    rent = Rent.create user_id: rent_params[:user_id],
+                       book_id: rent_params[:book_id],
+                       starting_date: rent_params[:from],
+                       ending_date: rent_params[:to]
+
+    render json: rent, status: :created
+  end
+
+  def rent_params
+    params.require(:rent)
+          .permit(:user_id, :book_id, :from, :to)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'users'
   get '/users/me', to: 'users#me'
-  resources :users, only: %i[index show]
+  resources :users, only: %i[index show] do
+    resources :rents, only: %i[index create]
+  end
   resources :books, only: %i[index show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
