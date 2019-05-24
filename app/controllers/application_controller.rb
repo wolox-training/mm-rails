62a5210a5d02e14df04281095b36add1c82186ef
Martín Mallea <martin.mallea@wolox.com.ar>
2019-05-24 15:43:02 -0300

    Using I18n.with_locale to set locale context when sending mail

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,21 +4,11 @@ class ApplicationController < ActionController::API
   include Pundit
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_locale, if: :user_signed_in?
 
   rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
   rescue_from Pundit::NotAuthorizedError, with: :render_unauthorized
 
   protected
-
-  def set_locale
-    I18n.locale = current_user_locale || I18n.default_locale
-  end
-
-  def current_user_locale
-    locale = current_user.locale.downcase.to_sym
-    I18n.available_locales.include?(locale) ? locale : nil
-  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name locale])
--- app/mailers/rent_mailer.rb
+++ app/mailers/rent_mailer.rb
@@ -2,6 +2,16 @@ class RentMailer < ApplicationMailer
   def notification_mail(rent)
     @rent = rent
     @user = @rent.user
-    mail(to: @user.email, subject: t('rent_notification_mail.subject'))
+
+    I18n.with_locale(user_locale(@user)) do
+      mail(to: @user.email, subject: t('rent_notification_mail.subject'))
+    end
+  end
+
+  private
+
+  def user_locale(user)
+    locale = user.locale.downcase.to_sym
+    I18n.available_locales.include?(locale) ? locale : I18n.default_locale
   end
 end
