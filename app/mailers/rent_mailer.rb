3de8d9518258fc42bbb4638df644040197447067
Lucas Voboril <lucas.voboril@wolox.com.ar>
2019-05-24 13:07:13 -0300

    Update didactic

--- app/mailers/rent_mailer.rb
+++ app/mailers/rent_mailer.rb
@@ -2,6 +2,6 @@ class RentMailer < ApplicationMailer
   def notification_mail(rent)
     @rent = rent
     @user = @rent.user
-    mail(to: @user.email, subject: 'Your rent has been registered')
+    mail(to: @user.email, subject: 'Your rent has_been registered')
   end
 end
