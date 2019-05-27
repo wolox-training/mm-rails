1b0fb5ca3984ff78105fd75ab56c3d27fd1f3c0c
Martín Mallea <martin.mallea@wolox.com.ar>
2019-05-27 17:49:37 -0300

    Parsing opnlibrary API response

--- app/services/openlibrary_service.rb
+++ app/services/openlibrary_service.rb
@@ -4,9 +4,31 @@ class OpenlibraryService
 
   default_params format: :json, jscmd: 'data'
 
+  class BookNotFoundError < StandardError
+  end
+
   def search_by_isbn(isbn)
-    isbn_string = "ISBN:#{isbn}"
+    @isbn = isbn
+    isbn_string = "ISBN:#{@isbn}"
     options = { query: { bibkeys: isbn_string } }
-    self.class.get('/books', options)
+    response = self.class.get('/books', options)
+
+    return parse_response_book(response[isbn_string]) if response.success?
+
+    raise BookNotFoundError, response.response
+  end
+
+  private
+
+  def parse_response_book(book)
+    {
+      isbn: @isbn,
+      title: book['title'],
+      subtitle: book['subtitle'],
+      number_of_pages: book['number_of_pages'],
+      authors: book['authors'].map { |author| author['name'] }
+    }
+  rescue StandardError
+    raise BookNotFoundError, 'Invalid response book'
   end
 end
