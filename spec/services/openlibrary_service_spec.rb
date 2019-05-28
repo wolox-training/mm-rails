56c20b0bc66d57df2cc992e246e89fd6bf07a7ca
Martín Mallea <martin.mallea@wolox.com.ar>
2019-05-28 17:59:10 -0300

    Adding failure test cases for OpenlibraryService

--- spec/services/openlibrary_service_spec.rb
+++ spec/services/openlibrary_service_spec.rb
@@ -5,23 +5,67 @@ describe OpenlibraryService do
   let(:service) { OpenlibraryService.new }
   let(:uri) { 'https://openlibrary.org/api/books' }
 
-  context 'when searching a book by isbn' do
-    before { successfuly_book_get }
+  describe '#search_by_isbn' do
+    subject { service.search_by_isbn(isbn) }
 
-    let(:isbn) { '0385472579' }
-    subject! { service.search_by_isbn(isbn) }
+    context 'when getting a successfuly response' do
+      before do
+        successfuly_book_get
+        subject
+      end
 
-    it 'makes an external web request' do
-      expect(WebMock).to have_requested(:get, "#{uri}?bibkeys=ISBN:#{isbn}&format=json&jscmd=data")
+      let(:isbn) { '0385472579' }
+
+      it 'makes an external web request' do
+        expect(WebMock)
+          .to have_requested(:get, "#{uri}?bibkeys=ISBN:#{isbn}&format=json&jscmd=data")
+          .once
+      end
+
+      it 'responses with a Hash' do
+        is_expected.to be_a Hash
+      end
+
+      it 'responses with a Hash with expected keys' do
+        expected_keys = %i[isbn title subtitle number_of_pages authors]
+        is_expected.to include(*expected_keys)
+      end
     end
 
-    it 'responses with a Hash' do
-      is_expected.to be_a Hash
+    context 'when getting an empty response' do
+      before { empty_book_get }
+
+      let(:isbn) { '12345' }
+
+      it 'makes an external web request' do
+        subject
+      rescue OpenlibraryService::BookNotFoundError
+        expect(WebMock)
+          .to have_requested(:get, "#{uri}?bibkeys=ISBN:#{isbn}&format=json&jscmd=data")
+          .once
+      end
+
+      it 'raises BookNotFoundError' do
+        expect { subject }.to raise_error OpenlibraryService::BookNotFoundError
+      end
     end
 
-    it 'responses with a Hash with expected keys' do
-      expected_keys = %i[isbn title subtitle number_of_pages authors]
-      is_expected.to include(*expected_keys)
+    context 'when getting an unsuccessfully response' do
+      before { internal_server_error_book_get }
+
+      let(:isbn) { '9780201485370' }
+
+      it 'makes an external web request' do
+        subject
+      rescue OpenlibraryService::BookNotFoundError
+        expect(WebMock)
+          .to have_requested(:get, "#{uri}?bibkeys=ISBN:#{isbn}&format=json&jscmd=data")
+          .once
+      end
+
+      it 'raises BookNotFoundError' do
+        expect { subject }.to raise_error OpenlibraryService::BookNotFoundError
+      end
     end
   end
 end
--- spec/support/mocks_and_stubbs/openlibrary_mock.rb
+++ spec/support/mocks_and_stubbs/openlibrary_mock.rb
@@ -12,4 +12,28 @@ module OpenlibraryMock
         body: File.read('./spec/support/fixtures/openlibrary/response_success.json')
       )
   end
+
+  def empty_book_get
+    stub_request(:get, "#{BASE_URI}/books")
+      .with(
+        query: { bibkeys: 'ISBN:12345', format: 'json', jscmd: 'data' }
+      )
+      .to_return(
+        status: 200,
+        headers: { 'Content-Type' => 'application/json' },
+        body: '{}'
+      )
+  end
+
+  def internal_server_error_book_get
+    stub_request(:get, "#{BASE_URI}/books")
+      .with(
+        query: { bibkeys: 'ISBN:9780201485370', format: 'json', jscmd: 'data' }
+      )
+      .to_return(
+        status: 200,
+        headers: { 'Content-Type' => 'application/json' },
+        body: '{}'
+      )
+  end
 end
