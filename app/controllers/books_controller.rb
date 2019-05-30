19f0845f0203276a08ed857c9efe1791b541cc06
Martín Mallea <martin.mallea@wolox.com.ar>
2019-05-30 14:03:32 -0300

    Moving open_library_information method to books_controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :open_library_information
 
   def index
     render_paginated Book::Reducer.apply(params), each_serializer: BookPreviewSerializer
@@ -7,5 +7,23 @@ class BooksController < ApplicationController
 
   def show
     render json: Book.find(params[:id])
+  end
+
+  def open_library_information
+    return render_errors('ISBN should be numeric', :bad_request) unless valid_isbn?
+
+    render json: OpenlibraryService.new.search_by_isbn(isbn), status: :ok
+  rescue OpenlibraryService::BookNotFoundError => e
+    render_errors(e.message, :not_found)
+  end
+
+  private
+
+  def isbn
+    @isbn ||= params[:isbn]
+  end
+
+  def valid_isbn?
+    isbn&.contains_only_digits?
   end
 end
