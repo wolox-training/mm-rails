a61b05f111bb79566767e6ca0164ba9aea11feab
Martín Mallea <martin.mallea@wolox.com.ar>
2019-05-31 16:09:24 -0300

    2Sending email when rent is expiring

--- app/mailers/rent_mailer.rb
+++ app/mailers/rent_mailer.rb
@@ -8,6 +8,14 @@ class RentMailer < ApplicationMailer
     end
   end
 
+  def expiration_mail(rent)
+    @rent = rent
+    user = rent.user
+    I18n.with_locale(user_locale(user)) do
+      mail(to: user.email, subject: t('rent_expiration_mail.title'))
+    end
+  end
+
   private
 
   def user_locale(user)
--- app/workers/check_expired_rents_worker.rb
+++ app/workers/check_expired_rents_worker.rb
@@ -1,7 +1,9 @@
 class CheckExpiredRentsWorker
   include Sidekiq::Worker
 
-  def perform(*args)
-    # Do something
+  def perform(*_args)
+    Rent.expiring_today.each do |rent|
+      RentMailer.expiration_mail(rent).deliver_later
+    end
   end
 end
