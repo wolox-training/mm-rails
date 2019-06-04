cea6a417b02ccbecea70649a6439cfe107e41b3b
Martín Mallea <martin.mallea@wolox.com.ar>
2019-06-04 15:00:05 -0300

    Moving DML to a task

--- db/migrate/20190531210309_limit_books_title.rb
+++ db/migrate/20190531210309_limit_books_title.rb
@@ -1,8 +1,5 @@
 class LimitBooksTitle < ActiveRecord::Migration[5.1]
   def up
-    Book.where('length(title) > 25').find_each do |book|
-      book.update title: book.title.first(22) + '...'
-    end
     change_column :books, :title, :string, limit: 25
   end
 
