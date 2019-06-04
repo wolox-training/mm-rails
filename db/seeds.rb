0e26ffd110bf89c02a63dc6f95b39dea2bd3af29
Martín Mallea <martin.mallea@wolox.com.ar>
2019-06-04 16:05:22 -0300

    Deleting comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 [*1..50_000].each do
   book = Book.create(author: Faker::Book.author, title: Faker::Book.title.first(25),
                      publisher: Faker::Book.publisher, year: Faker::Number.positive.truncate.to_s,
